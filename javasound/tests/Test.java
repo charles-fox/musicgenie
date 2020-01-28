6175061753ebbc8ed1e0a644c09b8e89a8fa7d72
Charles Fox <charles.fox@gmail.com>
2020-01-28 22:25:19 +0000

    can now make and save midi file again; to play in fluidsynth

--- javasound/tests/Test.java
+++ javasound/tests/Test.java
@@ -1,108 +1,68 @@
-//test
+//sudo apt install fludsynth
+//sudo apt install fluid-soundfont-gm
+//fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:1 /usr/share/sounds/sf2/FluidR3_GM.sf2 mid.mid
 
 import javax.sound.midi.*;
 import java.net.*;
 import java.io.*;
 
-
 public class Test {
-
-
 
  	public static void main(String args[]) throws MalformedURLException, InvalidMidiDataException, IOException, MidiUnavailableException {
 
-
-
-		System.out.println("hello");
-
-		//get a sequencer
-		Sequencer sequencer = MidiSystem.getSequencer();
-
-		sequencer.open();
-
-		//make an output stream to a file so we can read the results
-		DataOutputStream out = new DataOutputStream(
-									new BufferedOutputStream (
-										new FileOutputStream("Data.txt")));
-
+		System.out.println("testing MIDI");
 
 		Sequence mySeq = new Sequence(Sequence.PPQ, 384);
-
 		Track trackZero = mySeq.createTrack();
 		Track newTrack = mySeq.createTrack();
-
-
-		//mySeq.add(trackZero);
-		//mySeq.add(newTrack);
-
 		//set voice
 		ShortMessage ve = new ShortMessage();
 		ve.setMessage(192, 58, 0);
 		newTrack.add( new MidiEvent(ve, 0l) );
-
 		//note 1 on
 		ShortMessage se3 = new ShortMessage();
 		se3.setMessage(144, 41, 127);
 		newTrack.add( new MidiEvent(se3, 0l) );
-
 		//note 1 off
 		ShortMessage se4 = new ShortMessage();
 		se4.setMessage(128, 41, 127);
 		newTrack.add(new MidiEvent(se4, 900l));
-
-
 		//set voice on other track
 		ShortMessage vez = new ShortMessage();
 		vez.setMessage(193, 4, 0);
 		newTrack.add(new MidiEvent(vez, 0l));
-
 		//note 2 on
 		ShortMessage se2 = new ShortMessage();
 		se2.setMessage(145, 48, 127);
 		trackZero.add(new MidiEvent(se2, 800l));
-
 		//note 2 off
 		ShortMessage se5 = new ShortMessage();
 		se5.setMessage(129, 48, 127);
 		trackZero.add(new MidiEvent (se5, 1600l));
 
-
-
-
-
-		//play the sequence
-		sequencer.setSequence(mySeq);
-		sequencer.start();
-
-		File file = new File("D:/js/tests/mid.mid");
-
-		/*MidiFileFormat format = new MidiFileFormat(
-					1,
-					  mySeq.getDivisionType(),
-                      mySeq.getResolution(),
-                      MidiFileFormat.UNKNOWN_LENGTH,
-                      MidiFileFormat.UNKNOWN_LENGTH
-                      );
-*/
+		File file = new File("out.mid");
 		int type = 1;
 		MidiSystem.write(mySeq, type, file);
 
+
+
+
+		//play the sequence  TODO 2020 not working
+		Sequencer sequencer = MidiSystem.getSequencer();
+		sequencer.open();
+		sequencer.setSequence(mySeq);
+		sequencer.start();
+
 		//wait for playing
-		for (int i=0; i<10000; i++) {p(""+i);}
+		for (int i=0; i<100000; i++) {p(""+i);}
 		p("finished playing");
 
-		//close the output file and midisystem
-		out.close();
 		sequencer.close();
-
 		System.exit(1);
 
 	}
 
-
 	private static void p(String s) {
 		System.out.println(s);
 	}
-
-
 }
--- src/uk/ac/cam/cwf22/mg/Test_MusicGenie.java
+++ src/uk/ac/cam/cwf22/mg/Test_MusicGenie.java
@@ -8,9 +8,15 @@ import uk.ac.cam.cwf22.mg.core.*;
 import uk.ac.cam.cwf22.mg.compiler.*;
 import uk.ac.cam.cwf22.mg.gui.*;
 import uk.ac.cam.cwf22.mg.graphics.*;
+
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
+
+import javax.sound.midi.*;
+import java.net.*;
+import java.io.*;
+
 
 public class Test_MusicGenie
 {
@@ -28,7 +34,8 @@ public class Test_MusicGenie
 		testKeyRoll();
 		//testAwt();
 		//test3D();
-		Test_MusicGenie t = new Test_MusicGenie();
+		testMidiFile();
+		//Test_MusicGenie t = new Test_MusicGenie();
 	}
 	
 	private static void testProperties() {
@@ -115,9 +122,65 @@ public class Test_MusicGenie
 
 
 
+//sudo apt install fludsynth
+//sudo apt install fluid-soundfont-gm
+//fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:1 /usr/share/sounds/sf2/FluidR3_GM.sf2 mid.mid
+
+
+        public static void testMidiFile() throws MalformedURLException, InvalidMidiDataException, IOException, MidiUnavailableException {
+
+                System.out.println("testing MIDI");
+
+                Sequence mySeq = new Sequence(Sequence.PPQ, 384);
+                Track trackZero = mySeq.createTrack();
+                Track newTrack = mySeq.createTrack();
+                //set voice
+                ShortMessage ve = new ShortMessage();
+                ve.setMessage(192, 58, 0);
+                newTrack.add( new MidiEvent(ve, 0l) );
+                //note 1 on
+                ShortMessage se3 = new ShortMessage();
+                se3.setMessage(144, 41, 127);
+                newTrack.add( new MidiEvent(se3, 0l) );
+                //note 1 off
+                ShortMessage se4 = new ShortMessage();
+                se4.setMessage(128, 41, 127);
+                newTrack.add(new MidiEvent(se4, 900l));
+                //set voice on other track
+                ShortMessage vez = new ShortMessage();
+                vez.setMessage(193, 4, 0);
+                newTrack.add(new MidiEvent(vez, 0l));
+                //note 2 on
+                ShortMessage se2 = new ShortMessage();
+                se2.setMessage(145, 48, 127);
+                trackZero.add(new MidiEvent(se2, 800l));
+                //note 2 off
+                ShortMessage se5 = new ShortMessage();
+                se5.setMessage(129, 48, 127);
+                trackZero.add(new MidiEvent (se5, 1600l));
+
+                File file = new File("out.mid");
+                int type = 1;
+                MidiSystem.write(mySeq, type, file);
+
+
+                //play the sequence  TODO 2020 not working
+                Sequencer sequencer = MidiSystem.getSequencer();
+                sequencer.open();
+                sequencer.setSequence(mySeq);
+                sequencer.start();
+
+                //wait for playing
+                for (int i=0; i<100000; i++) {p(""+i);}
+                p("finished playing");
+
+                sequencer.close();
+                System.exit(1);
+	}
+
+
 
 	//=======================
-	
 	/** shorthand for system.out.println
 	 */
 	private static void p(String s) {
